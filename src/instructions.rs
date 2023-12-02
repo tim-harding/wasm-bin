46cb5b46ed02724b2045cabaa332d0a9eaa203df
Tim Harding <tim@timharding.co>
2023-12-01 17:47:36 -0800

    Use write_all to simplify instructions

--- src/instructions.rs
+++ src/instructions.rs
@@ -4,7 +4,7 @@ use crate::{
     modules::{Dataidx, Elemidx, Funcidx, Globalidx, Labelidx, Localidx, Tableidx, Typeidx},
     types::{Reftype, Valtype},
     values::S33,
-    Grammar, Vector,
+    write_all, Grammar, Vector,
 };
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -96,29 +96,7 @@ pub enum Instr {
     TableSize(Tableidx),
     TableFill(Tableidx),
     // Memory
-    I32Load(Memarg),
-    I64Load(Memarg),
-    F32Load(Memarg),
-    F64Load(Memarg),
-    I32Load8S(Memarg),
-    I32Load8U(Memarg),
-    I32Load16S(Memarg),
-    I32Load16U(Memarg),
-    I64Load8S(Memarg),
-    I64Load8U(Memarg),
-    I64Load16S(Memarg),
-    I64Load16U(Memarg),
-    I64Load32S(Memarg),
-    I64Load32U(Memarg),
-    I32Store(Memarg),
-    I64Store(Memarg),
-    F32Store(Memarg),
-    F64Store(Memarg),
-    I32Store8(Memarg),
-    I32Store16(Memarg),
-    I64Store8(Memarg),
-    I64Store16(Memarg),
-    I64Store32(Memarg),
+    MemoryMemarg(MemoryMemarg, Memarg),
     MemorySize,
     MemoryGrow,
     MemoryInit(Dataidx),
@@ -148,321 +126,108 @@ impl Grammar for Instr {
             // Control
             Instr::Unreachable => 0x00u8.write(w),
             Instr::Nop => 0x01u8.write(w),
-            Instr::Block(bt, instructions) => {
-                0x02u8.write(w)?;
-                bt.write(w)?;
-                instructions
-                    .iter()
-                    .map(|i| i.write(w))
-                    .collect::<io::Result<()>>()?;
-                0x0bu8.write(w)
-            }
-            Instr::Loop(bt, instructions) => {
-                0x03u8.write(w)?;
-                bt.write(w)?;
-                instructions
-                    .iter()
-                    .map(|i| i.write(w))
-                    .collect::<io::Result<()>>()?;
-                0x0bu8.write(w)
-            }
-            Instr::If(bt, instructions) => {
-                0x04u8.write(w)?;
-                bt.write(w)?;
-                instructions
-                    .iter()
-                    .map(|i| i.write(w))
-                    .collect::<io::Result<()>>()?;
-                0x0bu8.write(w)
-            }
-            Instr::IfElse(bt, in1, in2) => {
-                0x04u8.write(w)?;
-                bt.write(w)?;
-                in1.iter().map(|i| i.write(w)).collect::<io::Result<()>>()?;
-                0x05u8.write(w)?;
-                in2.iter().map(|i| i.write(w)).collect::<io::Result<()>>()?;
-                0x0bu8.write(w)
-            }
-            Instr::Br(l) => {
-                0x0cu8.write(w)?;
-                l.write(w)
-            }
-            Instr::BrIf(l) => {
-                0x0du8.write(w)?;
-                l.write(w)
-            }
-            Instr::BrTable(l, default) => {
-                0x0eu8.write(w)?;
-                l.write(w)?;
-                default.write(w)
-            }
+            Instr::Block(bt, r#in) => write_all!(w, 0x02u8, bt, r#in, 0x0bu8),
+            Instr::Loop(bt, r#in) => write_all!(w, 0x03u8, bt, r#in, 0x0bu8),
+            Instr::If(bt, r#in) => write_all!(w, 0x04u8, bt, r#in, 0x0bu8),
+            Instr::IfElse(bt, in1, in2) => write_all!(w, 0x04u8, bt, in1, 0x05u8, in2, 0x0bu8),
+            Instr::Br(l) => write_all!(w, 0x0cu8, l),
+            Instr::BrIf(l) => write_all!(w, 0x0du8, l),
+            Instr::BrTable(l, default) => write_all!(w, 0x0eu8, l, default),
             Instr::Return => 0x0fu8.write(w),
-            Instr::Call(f) => {
-                0x10u8.write(w)?;
-                f.write(w)
-            }
-            Instr::CallIndirect(ty, table) => {
-                0x11u8.write(w)?;
-                ty.write(w)?;
-                table.write(w)
-            }
+            Instr::Call(x) => write_all!(w, 0x10u8, x),
+            Instr::CallIndirect(y, x) => write_all!(w, 0x11u8, y, x),
 
             // Reference
-            Instr::RefNull(t) => {
-                0xd0u8.write(w)?;
-                t.write(w)
-            }
+            Instr::RefNull(t) => write_all!(w, 0xd0u8, t),
             Instr::RefIsNull => 0xd1u8.write(w),
-            Instr::RefFunc(f) => {
-                0xd2u8.write(w)?;
-                f.write(w)
-            }
+            Instr::RefFunc(x) => write_all!(w, 0xd2u8, x),
 
             // Parametric
             Instr::Drop => 0x1au8.write(w),
-            Instr::Select(ty) => {
-                if let Some(ty) = ty {
-                    0x1cu8.write(w)?;
-                    ty.write(w)
+            Instr::Select(t) => {
+                if let Some(t) = t {
+                    write_all!(w, 0x1cu8, t)
                 } else {
                     0x1bu8.write(w)
                 }
             }
 
             // Variable
-            Instr::LocalGet(x) => {
-                0x20u8.write(w)?;
-                x.write(w)
-            }
-            Instr::LocalSet(x) => {
-                0x21u8.write(w)?;
-                x.write(w)
-            }
-            Instr::LocalTee(x) => {
-                0x22u8.write(w)?;
-                x.write(w)
-            }
-            Instr::GlobalGet(x) => {
-                0x23u8.write(w)?;
-                x.write(w)
-            }
-            Instr::GlobalSet(x) => {
-                0x24u8.write(w)?;
-                x.write(w)
-            }
+            Instr::LocalGet(x) => write_all!(w, 0x20u8, x),
+            Instr::LocalSet(x) => write_all!(w, 0x21u8, x),
+            Instr::LocalTee(x) => write_all!(w, 0x22u8, x),
+            Instr::GlobalGet(x) => write_all!(w, 0x23u8, x),
+            Instr::GlobalSet(x) => write_all!(w, 0x24u8, x),
 
             // Table
-            Instr::TableGet(table) => {
-                0x25u8.write(w)?;
-                table.write(w)
-            }
-            Instr::TableSet(table) => {
-                0x26u8.write(w)?;
-                table.write(w)
-            }
-            Instr::TableInit(element, table) => {
-                0xfcu8.write(w)?;
-                12u32.write(w)?;
-                element.write(w)?;
-                table.write(w)
-            }
-            Instr::ElemDrop(element) => {
-                0xfcu8.write(w)?;
-                13u32.write(w)?;
-                element.write(w)
-            }
-            Instr::TableCopy(dst, src) => {
-                0xfcu8.write(w)?;
-                14u32.write(w)?;
-                dst.write(w)?;
-                src.write(w)
-            }
-            Instr::TableGrow(table) => {
-                0xfcu8.write(w)?;
-                15u32.write(w)?;
-                table.write(w)
-            }
-            Instr::TableSize(table) => {
-                0xfcu8.write(w)?;
-                16u32.write(w)?;
-                table.write(w)
-            }
-            Instr::TableFill(table) => {
-                0xfcu8.write(w)?;
-                17u32.write(w)?;
-                table.write(w)
-            }
+            Instr::TableGet(x) => write_all!(w, 0x25u8, x),
+            Instr::TableSet(x) => write_all!(w, 0x26u8, x),
+            Instr::TableInit(y, x) => write_all!(w, 0xfcu8, 12u32, y, x),
+            Instr::ElemDrop(x) => write_all!(w, 0xfcu8, 13u32, x),
+            Instr::TableCopy(x, y) => write_all!(w, 0xfcu8, 14u32, x, y),
+            Instr::TableGrow(x) => write_all!(w, 0xfcu8, 15u32, x),
+            Instr::TableSize(x) => write_all!(w, 0xfcu8, 16u32, x),
+            Instr::TableFill(x) => write_all!(w, 0xfcu8, 17u32, x),
 
             // Memory
-            Instr::I32Load(m) => {
-                0x28u8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Load(m) => {
-                0x29u8.write(w)?;
-                m.write(w)
-            }
-            Instr::F32Load(m) => {
-                0x2au8.write(w)?;
-                m.write(w)
-            }
-            Instr::F64Load(m) => {
-                0x2bu8.write(w)?;
-                m.write(w)
-            }
-            Instr::I32Load8S(m) => {
-                0x2cu8.write(w)?;
-                m.write(w)
-            }
-            Instr::I32Load8U(m) => {
-                0x2du8.write(w)?;
-                m.write(w)
-            }
-            Instr::I32Load16S(m) => {
-                0x2eu8.write(w)?;
-                m.write(w)
-            }
-            Instr::I32Load16U(m) => {
-                0x2fu8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Load8S(m) => {
-                0x30u8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Load8U(m) => {
-                0x31u8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Load16S(m) => {
-                0x32u8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Load16U(m) => {
-                0x33u8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Load32S(m) => {
-                0x34u8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Load32U(m) => {
-                0x35u8.write(w)?;
-                m.write(w)
-            }
-            Instr::I32Store(m) => {
-                0x36u8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Store(m) => {
-                0x37u8.write(w)?;
-                m.write(w)
-            }
-            Instr::F32Store(m) => {
-                0x38u8.write(w)?;
-                m.write(w)
-            }
-            Instr::F64Store(m) => {
-                0x39u8.write(w)?;
-                m.write(w)
-            }
-            Instr::I32Store8(m) => {
-                0x3au8.write(w)?;
-                m.write(w)
-            }
-            Instr::I32Store16(m) => {
-                0x3bu8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Store8(m) => {
-                0x3cu8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Store16(m) => {
-                0x3du8.write(w)?;
-                m.write(w)
-            }
-            Instr::I64Store32(m) => {
-                0x3eu8.write(w)?;
-                m.write(w)
-            }
+            Instr::MemoryMemarg(op, m) => write_all!(w, op, m),
             Instr::MemorySize => w.write_all(&[0x3f, 0x00]),
             Instr::MemoryGrow => w.write_all(&[0x40, 0x00]),
-            Instr::MemoryInit(idx) => {
-                0xfcu8.write(w)?;
-                8u32.write(w)?;
-                idx.write(w)?;
-                0x00.write(w)
-            }
-            Instr::DataDrop(idx) => {
-                0xfcu8.write(w)?;
-                9u32.write(w)?;
-                idx.write(w)
-            }
-            Instr::MemoryCopy => {
-                0xfcu8.write(w)?;
-                10u32.write(w)?;
-                w.write_all(&[0x00, 0x00])
-            }
-            Instr::MemoryFill => {
-                0xfcu8.write(w)?;
-                11u32.write(w)?;
-                0x00u8.write(w)
-            }
-            Instr::I32Const(n) => {
-                0x41u8.write(w)?;
-                n.write(w)
-            }
-            Instr::I64Const(n) => {
-                0x42u8.write(w)?;
-                n.write(w)
-            }
-            Instr::F32Const(n) => {
-                0x43u8.write(w)?;
-                n.write(w)
-            }
-            Instr::F64Const(n) => {
-                0x44u8.write(w)?;
-                n.write(w)
-            }
-            Instr::Numeric(opcode) => (*opcode as u8).write(w),
-            Instr::TruncSat(opcode) => {
-                0xfcu8.write(w)?;
-                (*opcode as u32).write(w)
-            }
+            Instr::MemoryInit(x) => write_all!(w, 0xfcu8, 8u32, x, 0x00),
+            Instr::DataDrop(x) => write_all!(w, 0xfcu8, 9u32, x),
+            Instr::MemoryCopy => write_all!(w, 0xfcu8, 10u32, 0x00u8, 0x00u8),
+            Instr::MemoryFill => write_all!(w, 0xfcu8, 11u32, 0x00u8),
+
+            // Numeric
+            Instr::I32Const(n) => write_all!(w, 0x41u8, n),
+            Instr::I64Const(n) => write_all!(w, 0x42u8, n),
+            Instr::F32Const(n) => write_all!(w, 0x43u8, n),
+            Instr::F64Const(n) => write_all!(w, 0x44u8, n),
+            Instr::Numeric(op) => op.write(w),
+            Instr::TruncSat(op) => write_all!(w, 0xfcu8, op),
 
             // Vector
-            Instr::VectorMemarg(opcode, m) => {
-                0xfdu8.write(w)?;
-                (*opcode as u32).write(w)?;
-                m.write(w)
-            }
-            Instr::VectorMemargLaneidx(opcode, m, l) => {
-                0xfdu8.write(w)?;
-                (*opcode as u32).write(w)?;
-                m.write(w)?;
-                l.write(w)
-            }
-            Instr::V128Const(b) => {
-                0xfdu8.write(w)?;
-                12u32.write(w)?;
-                b.iter().map(|b| b.write(w)).collect()
-            }
-            Instr::I8x16Shuffle(l) => {
-                0xfdu8.write(w)?;
-                13u32.write(w)?;
-                l.iter().map(|l| l.write(w)).collect()
-            }
-            Instr::VectorLaneidx(opcode, lane) => {
-                0xfdu8.write(w)?;
-                (*opcode as u32).write(w)?;
-                lane.write(w)
-            }
-            Instr::VectorNoImmediate(opcode) => {
-                0xfdu8.write(w)?;
-                (*opcode as u32).write(w)
-            }
+            Instr::VectorMemarg(op, m) => write_all!(w, 0xfdu8, op, m),
+            Instr::VectorMemargLaneidx(op, m, l) => write_all!(w, 0xfdu8, op, m, l),
+            Instr::V128Const(b) => write_all!(w, 0xfdu8, 12u32, b.as_slice()),
+            Instr::I8x16Shuffle(l) => write_all!(w, 0xfdu8, 13u32, l.as_slice()),
+            Instr::VectorLaneidx(op, l) => write_all!(w, 0xfdu8, op, l),
+            Instr::VectorNoImmediate(op) => write_all!(w, 0xfdu8, op),
         }
+    }
+}
+
+#[repr(u8)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum MemoryMemarg {
+    I32Load = 0x28,
+    I64Load,
+    F32Load,
+    F64Load,
+    I32Load8S,
+    I32Load8U,
+    I32Load16S,
+    I32Load16U,
+    I64Load8S,
+    I64Load8U,
+    I64Load16S,
+    I64Load16U,
+    I64Load32S,
+    I64Load32U,
+    I32Store,
+    I64Store,
+    F32Store,
+    F64Store,
+    I32Store8,
+    I32Store16,
+    I64Store8,
+    I64Store16,
+    I64Store32,
+}
+
+impl Grammar for MemoryMemarg {
+    fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        todo!()
     }
 }
 
@@ -599,6 +364,12 @@ pub enum Numeric {
     I64Extend32S,
 }
 
+impl Grammar for Numeric {
+    fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        (*self as u8).write(w)
+    }
+}
+
 #[repr(u32)]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum TruncSat {
@@ -610,6 +381,12 @@ pub enum TruncSat {
     I64TruncSatF32U,
     I64TruncSatF64S,
     I64TruncSatF64U,
+}
+
+impl Grammar for TruncSat {
+    fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        (*self as u32).write(w)
+    }
 }
 
 #[repr(u32)]
@@ -631,6 +408,12 @@ pub enum VectorMemarg {
     V128Store = 11,
 }
 
+impl Grammar for VectorMemarg {
+    fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        (*self as u32).write(w)
+    }
+}
+
 #[repr(u32)]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum VectorMemargLaneidx {
@@ -642,6 +425,12 @@ pub enum VectorMemargLaneidx {
     V128Store16Lane,
     V128Store32Lane,
     V128Store64Lane,
+}
+
+impl Grammar for VectorMemargLaneidx {
+    fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        (*self as u32).write(w)
+    }
 }
 
 #[repr(u32)]
@@ -661,6 +450,12 @@ pub enum VectorLaneidx {
     F32x4ReplaceLane,
     F64x2ExtractLane,
     F64x2ReplaceLane,
+}
+
+impl Grammar for VectorLaneidx {
+    fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        (*self as u32).write(w)
+    }
 }
 
 #[repr(u32)]
@@ -881,4 +676,10 @@ pub enum VectorNoImmediate {
     F64x2ConvertLowI32x4U,
     F32x4DemoteF64x2Zero = 94,
     F64x2PromoteLowF32x4,
+}
+
+impl Grammar for VectorNoImmediate {
+    fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        (*self as u32).write(w)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,16 @@ pub trait Grammar {
     fn write<W: Write>(&self, w: &mut W) -> io::Result<()>;
 }
 
+#[macro_export]
+macro_rules! write_all {
+    ($w:expr, $($e:expr),*) => {
+        {
+            $($e.write($w)?;)*
+            Ok(())
+        }
+    };
+}
+
 // https://webassembly.github.io/spec/core/binary/conventions.html#vectors
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Vector<T>(pub Box<[T]>);
@@ -19,6 +29,24 @@ where
 {
     fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
         (self.0.len() as u32).write(w)?;
-        self.0.into_iter().map(|t| t.write(w)).collect()
+        self.0.as_ref().write(w)
+    }
+}
+
+impl<T> Grammar for &[T]
+where
+    T: Grammar,
+{
+    fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        self.iter().map(|i| i.write(w)).collect()
+    }
+}
+
+impl<T> Grammar for Box<[T]>
+where
+    T: Grammar,
+{
+    fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+        self.as_ref().write(w)
     }
 }
